0a3438d933af2691469e262509d3a53c34b08f4d
Jelena Chuklina <chuklina.jelena@gmail.com>
2017-07-12 09:00:29 +0200

    renamed read.config into more conventional naming pattern, solved data frame merging and cleaning bug in read_data.R

--- R/read_data.R
+++ R/read_data.R
@@ -5,15 +5,18 @@
 #' @param response.file if not NULL, the response table must be in this file
 #' @param match.column the column (columns?)
 #'
-#' @return the data.table with data and the colum that gives the information about the response
+#' @return the data.table with data and the column that gives the information about the response
 #' @export
 #'
 #' @examples
-read.data <- function(data.file, response.column, response.file = NULL, match.column = NULL){
+#' importFrom data.table fread
+read_data <- function(data.file, response.column, response.file = NULL, match.column = NULL){
   if (!is.null(response.file)){
     x = fread(data.file, header = T)
     y = fread(response.file, header = T)
-    data = merge(x, y, by = match_column) %>% select_(-one_of(match_column))
+    data = merge(x, y, by = match.column) %>%
+      select(-one_of(setdiff(names(y), response.column))) %>%
+      rename_('.outcome' = response.column)
   } else {
     if (is.null(match.column)){
       stop('Column(s) to match predictor file and response file not provided')
@@ -21,5 +24,5 @@ read.data <- function(data.file, response.column, response.file = NULL, match.co
       data = fread(data.file, header = T)
     }
   }
-  return(list(data = data, response.column = reponse.column))
+  return(list(data = data, response.column = response.column))
 }
